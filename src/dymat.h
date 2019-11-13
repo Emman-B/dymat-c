89788bd74e46bbd7f17fb6cc4679e76804722bd7
Emmanuel Butor <emmanuelbutor.mehs@gmail.com>
2019-11-13 02:38:42 -0800

    Updates to debugging functions in dymat.h, adding node getter functions; also changed ctor_node return type (void -> node)

--- src/dymat.h
+++ src/dymat.h
@@ -1,11 +1,12 @@
-#ifndef _DYMAT_H
-#define _DYMAT_H
+#ifndef DYMAT_H
+#define DYMAT_H
 
 #include<stdlib.h>
+#include<stdio.h>
 
 // dynamic memory allocation tracker object
 typedef struct dymat_obj* dymat;
-typedef struct node_obj* node;      // identify node
+typedef struct node_obj* node;      // primarily for debugging, not intended to be accessed
 
 // malloc which stores pointer to queue and returns it
 // "Generic Pointer" is used as description if desc not defined
@@ -20,8 +21,17 @@ void* td_calloc(char* desc, size_t number, size_t sz);
 int t_free(void* memptr);
 
 // functions for debugging table/node functions
-#if defined(_DYMAT_ENABLE_DEBUG)
-    void ctor_node(void* memptr, char* desc, size_t sz);
+#ifdef DYMAT_ENABLE_DEBUG
+    // node access functions
+    void** get_memptr (node n);
+    char* get_desc (node n);
+    node get_next(node n);
+    #ifdef DYMAT_MEMRPT
+    int get_size (node n);
+    int get_status (node n);
+    #endif
+
+    node ctor_node(void* memptr, char* desc, size_t sz);
     int node_equals(node nodeA, node nodeB);
     void add_node(node newnode);
     void remove_node(node deletenode);
